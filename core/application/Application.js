362851f4420bcd1c03d261aabcfb922970b3d07e
himanshu <iamhimashu7102@gmail.com>
2025-09-06 22:42:10 +0530

    Added link for ease

--- core/application/Application.js
+++ core/application/Application.js
@@ -5,7 +5,7 @@ class Application extends BaseApplication {
     run() {
         const port = this.configs.port || 3000;
         this.express.listen(port, () => {
-            console.log(`Application listening on port: ${port}`);
+            console.log(`Application listening on port: ${port} http://localhost:${port}`);
         })
     }
 }
